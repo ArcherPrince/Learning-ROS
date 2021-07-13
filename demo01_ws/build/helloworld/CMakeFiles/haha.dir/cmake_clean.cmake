e10d5c0eca986494e87bc5a54c6902ed588543b7
ArcherPrince <494292204@qq.com>
2021-07-13 23:05:18 +0800

    add 2 lessons

---
+++ demo01_ws/build/helloworld/CMakeFiles/haha.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/haha.dir/src/helloworld_c.cpp.o"
+  "/home/prince/LearningROS/demo01_ws/devel/lib/helloworld/haha.pdb"
+  "/home/prince/LearningROS/demo01_ws/devel/lib/helloworld/haha"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/haha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
